0a472f217cd81aa086d703d8278b924f67badefc
Tristan Zine <zine96@protonmail.com>
2022-02-02 16:42:10 -0500

    -Successfully connects to IMDb API -Gets top 250TV shows data -Uses Secret file to hide API key from being visible on github -Gets user rating data for rank 1, 50, 100, 200 TV shows along with The Wheel of Times user ratings.

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import requests
+import secrets
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def api_connect():
+    return requests.get('https://imdb-api.com/API/Top250TVs/' + secrets.api_key)
+
+
+def get_user_data(movie_id):
+    return requests.get('https://imdb-api.com/en/API/UserRatings/' + secrets.api_key + '/' + movie_id)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    imdb_API = api_connect()
+    print(imdb_API.status_code)
+    print(imdb_API.text)
+    print(imdb_API.headers)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    rank1_rated_show = get_user_data('tt5491994')
+    print(rank1_rated_show.text)
+    rank50_rated_show = get_user_data('tt2297757')
+    print(rank50_rated_show.text)
+    rank100_rated_show = get_user_data('tt0286486')
+    print(rank100_rated_show.text)
+    rank200_rated_show = get_user_data('tt1492966')
+    print(rank200_rated_show.text)
+    wheel_of_time_rating = get_user_data('tt7462410')
+    print(wheel_of_time_rating.text)
+
+# https://alexwlchan.net/2017/10/requests-hooks/
+# https://docs.python-requests.org/en/latest/user/advanced/#advanced
