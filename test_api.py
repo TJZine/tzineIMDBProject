ecc72fa3d7400e48e7a8a50526b5cda971054ce6
Tristan Zine <zine96@protonmail.com>
2022-02-02 18:19:03 -0500

    Fixed formatting errors

--- test_api.py
+++ test_api.py
@@ -1,10 +1,13 @@
 import requests
 import secrets
 
+
 def get_user_data(movie_id):
     return requests.get('https://imdb-api.com/en/API/UserRatings/' + secrets.api_key + '/' + movie_id)
+
 
 def get_show_test():
     show_response = get_user_data('tt8420184')
     response_content = show_response.json()
     assert response_content["rank"] == "16"
+
