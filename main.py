f825bc7ccd194907bf9f41e7d4812a26bf2059e4
Tristan Zine <zine96@protonmail.com>
2022-02-02 23:11:18 -0500

    -formatting and comments

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import requests
 import secrets
 
 
-def api_connect():
+def api_connect(): #connects to the IMDB top 250TV shows api
     return requests.get('https://imdb-api.com/API/Top250TVs/' + secrets.api_key)
 
 
-def get_user_data(movie_id):
+def get_user_data(movie_id):  #takes imdbID and returns user rating data
     return requests.get('https://imdb-api.com/en/API/UserRatings/' + secrets.api_key + '/' + movie_id)
 
 
@@ -19,12 +19,13 @@ if __name__ == '__main__':
     eraseFile = open("output/shows.txt", "w")  # erases any previous text in the file
     eraseFile.close()
 
-    imdb_API = api_connect()
+    imdb_API = api_connect()  # connect to imdb API
     print(imdb_API.status_code)
 
+    # prints rank 1,50,100,200 and wheels of time user ratings to shows.txt and console for testing
     write_to_file("User Ratings:" + "\n")
     rank1_rated_show = get_user_data('tt5491994')
-    write_to_file(str(rank1_rated_show.json()) + "\n")
+    write_to_file(str(rank1_rated_show.json()) + "\n")  # convert dictionary entry to string to write to shows.txt
     print(rank1_rated_show.json())
     rank50_rated_show = get_user_data('tt2297757')
     write_to_file(str(rank50_rated_show.json()) + "\n")
@@ -38,11 +39,12 @@ if __name__ == '__main__':
     wheel_of_time_rating = get_user_data('tt7462410')
     write_to_file(str(wheel_of_time_rating.json()) + "\n")
     print(wheel_of_time_rating.json())
+
+    # writing top 250 shows to console and shows.txt
     write_to_file("Top 250 Shows:" + "\n")
     text_value = imdb_API.text
     value_list = text_value.split('},{')
     value_list[0] = value_list[0][11:]
-    print(value_list[0])
     for n in value_list:
         print(n)
         write_to_file(n + "\n")
